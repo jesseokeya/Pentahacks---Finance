02d0e0cb0d041a92bf44d4365897933534cd68b6
jesseokeya <jesseokeya@gmail.com>
2018-11-17 15:09:54 -0500

    changes

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.get(['/', '/dashboard'], (_, res) => {
-  res.send('views/index.html')
+  res.send(__dirname + '/views/index.html')
 })
 
 app.get('/login', (_, res) => {
