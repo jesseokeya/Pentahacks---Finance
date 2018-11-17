d870938a7b7685e6dcc8a3bead69ab1eb1249a2e
jesseokeya <jesseokeya@gmail.com>
2018-11-17 14:18:39 -0500

    changes

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
+const dotenv = require('dotenv')
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose');
 
+dotenv.config()
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-mongoose.connect(_db, {useMongoClient: true});
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
 mongoose.Promise = global.Promise;
 
 app.use(cors());
